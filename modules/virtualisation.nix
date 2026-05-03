b8e46d5a427c8e22bec186008e19ca64759aae2c
Jaan Jahilo <jaanjahilo@outlook.com>
2026-05-03 14:15:30 +0300

    Fix using ethernet inside VM

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -8,6 +8,14 @@
       swtpm.enable = true; # TPM emulation (needed for Windows 11)
     };
   };
+  # Allow internet connection inside VM
+  virtualisation.libvirtd.allowedBridges = [ "virbr0" ];
+  networking.firewall.interfaces."virbr0" = {
+    allowedUDPPorts = [
+      67
+      68
+    ];
+  };
 
   virtualisation.spiceUSBRedirection.enable = true;
   programs.virt-manager.enable = true;
